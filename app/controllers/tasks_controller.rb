bfe6dd80d0d994126aaf1279003fc7a65f9634ab
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-08-17 17:46:58 +0000

    Add ability to toggle task completion status.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,18 @@
 class TasksController < ApplicationController
-def index
-  render json: Task.all
-end
+  def index
+    render json: Task.all
+  end
 
-def update
-  task = Task.find(params[:id])
-  task.update_attributes(task_params)
-  render json: task
-end
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    render json: task
+  end
 
-private
+  private
 
-def task_params
-  params.require(:task).permit(:done)
-end
+  def task_params
+    params.require(:task).permit(:done)
+  end
 
 end
