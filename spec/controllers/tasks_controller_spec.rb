408dd60430a6a323ffdcb2b9f0d572784a4ffd4a
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-08-21 21:10:23 +0000

    Perform AJAX request on submission.

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do 
       task1 = FactoryGirl.create(:task)
       task2 = FactoryGirl.create(:task)
-      task1.update_attributes(title: "Something else")
+      task1.update_attributes( title: "Something else" )
       get :index
       expect(response).to have_http_status(:success)
       response_value = ActiveSupport::JSON.decode(@response.body)
@@ -25,4 +25,13 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "tasks#create" do 
+    it "should allow new tasks to be created" do 
+      post :create, params: { task: { title: "Fix things" } }
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value["title"]).to eq( "Fix things" )
+      expect(Task.last.title).to eq( "Fix things" )
+    end
+  end
 end
